d5e498d5e1856bd8927bbca6c09435b9c10beb3e
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2023-06-13 14:29:10 +0700

    Update index.js
    edit host db

--- DB/index.js
+++ DB/index.js
@@ -1,27 +1,23 @@
 const mysql = require('mysql');
 
 
-<<<<<<< HEAD
 const db = mysql.createPool({
-=======
+
 const db = mysql.createConnection({
->>>>>>> abf44b5d2c576c9cdc3f0370cff3406e0e82eeac
-    host: 'chbwapp001',
+
+    host: 'yourHost',
     user: 'root',
-    password: 'Maxim123;',
-    database:'it_inventory',
+    password: 'password DB',
+    database:'DB name',
     port: '3306'
 });
 
-<<<<<<< HEAD
 
-=======
 db.connect((err) => {
     if (err){
         console.log('Error connect to DB is',err)
     }
     console.log('Mysql successfully connected');
 })
->>>>>>> abf44b5d2c576c9cdc3f0370cff3406e0e82eeac
 
 module.exports = db;
